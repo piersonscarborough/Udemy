26405c21622de74b2590b37a81fca5a6bb9bffb6
Pierson Scarborough <piersonscarborough@Piersons-MacBook-Pro.local>
2019-05-09 22:34:29 -0400

    changes to arrays.js

--- JS/arrays.js
+++ JS/arrays.js
@@ -3,6 +3,7 @@
 function printReverse(arr){
     for(var i = arr.length -1; i >= 0; i --){
         console.log(arr[i]);
+        return (arr[i]);
     }
 }
 printReverse([1,2,3,4]);
@@ -24,20 +25,22 @@ isUniform([1,1,1]);
 
 //Sum of the array
 function sumArray(arr){
-    total = 0;
+    var total = 0;
     arr.forEach(function(num){
         total = total + num
     });
+    return total;
     console.log(total);
 }
 
 sumArray([1,2,3]);
 
 function sumArray(arr){
-    total = 0;
+    var total = 0;
     for(var i = 0; i < arr.length; i ++){
         total += arr[i]
     }
+    return total;
     console.log(total);
 }
 
@@ -46,24 +49,26 @@ sumArray([1,10,3]);
 
 //Largest number in an array
 function max(arr){
-    highest = 0
+    var highest = 0
     arr.forEach(function(num){
         if (num > highest){
             highest = num
         } 
     });
+    return highest;
     console.log(highest);
 }
 
 max([0,3,5,4,5])
 
 function max(arr){
-    highest = 0
+    var highest = 0
     for(var i = 0; i < arr.length -1; i ++){
         if(arr[i] > highest){
             highest = arr[i]
         }
     }
+    return highest;
     console.log(highest);
 }
 
